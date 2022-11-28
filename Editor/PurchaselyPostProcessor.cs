02229c4cbfbe598fb1d52ff1312f5df4cdbf5fa8
Andriy Marshalek <amarshalek@ninevastudios.com>
2022-11-28 11:31:43 +0200

    switch to framework instead of pods, fix framework compilation error

---
+++ Editor/PurchaselyPostProcessor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace Purchasely.Editor
+{
+	public class PurchaselyPostProcessor
+	{
+		[PostProcessBuild]
+		public static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath)
+		{
+			if (buildTarget == BuildTarget.iOS)
+			{
+				var projPath = PBXProject.GetPBXProjectPath(buildPath);
+				var proj = new PBXProject();
+				proj.ReadFromFile(projPath);
+
+				//// Configure build settings
+				proj.SetBuildProperty(proj.GetUnityMainTargetGuid(), "ENABLE_BITCODE", "NO");
+				proj.SetBuildProperty(proj.GetUnityFrameworkTargetGuid(), "ENABLE_BITCODE", "NO");
+
+				proj.WriteToFile(projPath);
+			}
+		}
+	}
+}

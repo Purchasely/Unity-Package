de2f4fc9e4dd642167179158d7920b70fbfc4a78
Andriy Marshalek <amarshalek@ninevastudios.com>
2022-11-28 13:37:35 +0200

    experiment with ios framework embedding

--- Editor/PurchaselyPostProcessor.cs
+++ Editor/PurchaselyPostProcessor.cs
@@ -1,6 +1,8 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
+using UnityEditor.iOS.Xcode.Extensions;
 
 namespace Purchasely.Editor
 {
@@ -12,14 +14,24 @@ namespace Purchasely.Editor
 			if (buildTarget == BuildTarget.iOS)
 			{
 				var projPath = PBXProject.GetPBXProjectPath(buildPath);
-				var proj = new PBXProject();
-				proj.ReadFromFile(projPath);
+				var project = new PBXProject();
+				project.ReadFromFile(projPath);
+
+				var targetGuid = project.GetUnityFrameworkTargetGuid();
 
 				//// Configure build settings
-				proj.SetBuildProperty(proj.GetUnityMainTargetGuid(), "ENABLE_BITCODE", "NO");
-				proj.SetBuildProperty(proj.GetUnityFrameworkTargetGuid(), "ENABLE_BITCODE", "NO");
+				project.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
 
-				proj.WriteToFile(projPath);
+				var frameworkPath = Path.Combine("com.purchasely.unity/Native/IOS", "Purchasely.framework");
+				var fileGuid = project.AddFile(frameworkPath, Path.Combine("Frameworks", frameworkPath), PBXSourceTree.Sdk);
+				project.AddFileToEmbedFrameworks(targetGuid, fileGuid);
+				
+				project.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks $(PROJECT_DIR)/lib/$(CONFIGURATION) $(inherited)");
+				project.AddBuildProperty(targetGuid, "FRAMEWORK_SEARCH_PATHS", "$(inherited) $(PROJECT_DIR) $(PROJECT_DIR)/Frameworks");
+				project.AddBuildProperty(targetGuid, "DYLIB_INSTALL_NAME_BASE", "@rpath");
+				project.AddBuildProperty(targetGuid, "LD_DYLIB_INSTALL_NAME", "@executable_path/../Frameworks/$(EXECUTABLE_PATH)");
+
+				project.WriteToFile(projPath);
 			}
 		}
 	}

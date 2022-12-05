a2974992de3d74cbd4a7b8b14257e722c05cca4b
Andriy Marshalek <amarshalek@ninevastudios.com>
2022-12-05 17:33:26 +0200

    add plan fields

--- Runtime/Entity/Plan.cs
+++ Runtime/Entity/Plan.cs
@@ -4,10 +4,22 @@
 	{
 		public string id;
 		public bool isVisible;
+		public bool hasIntroductoryPrice;
+		public bool hasFreeTrial;
 		public int level;
+		public int amount;
+		public int introAmount;
 		public string name;
 		public string storeProductId;
 		public string type;
 		public string vendorId;
+		public string localizedAmount;
+		public string introPrice;
+		public string period;
+		public string productId;
+		public string currencySymbol;
+		public string introPeriod;
+		public string introDuration;
+		public string price;
 	}
 }

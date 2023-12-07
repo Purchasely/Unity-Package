37ec56a4f969adf84c7663fbd2722389a7d4d403
chouaibMo <chouaib@purchasely.com>
2023-12-07 14:29:32 +0100

    Upgrade android SDK to 4.2.0

--- Editor/PurchaselyPostProcessor.cs
+++ Editor/PurchaselyPostProcessor.cs
@@ -81,7 +81,7 @@ namespace Purchasely.Editor
 		private void AddPurchaselyDependencies(string builtProjectPath)
 		{
 			const string ERROR_MESSAGE = "Could not add Purchasely dependencies to build.gradle file.";
-			const string ANDROID_SDK_VERSION = "4.1.1";
+			const string ANDROID_SDK_VERSION = "4.2.0";
 
 			var buildGradleFilePath = Path.Combine(builtProjectPath, "build.gradle");
 			if (File.Exists(buildGradleFilePath))
